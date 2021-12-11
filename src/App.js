9ba53fc10c58ecd4fbc85f8c07b71a3c83e56ece
Aviad Mizrachi <aviadm@gmail.com>
2021-12-11 22:00:10 +0200

    updated package and added authorize docs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useAuth, useAuthUser } from '@frontegg/react'
-import {useHistory} from "react-router-dom";
+import {useAuthUser, useTenantsState, useAuthActions} from '@frontegg/react'
+import { useHistory } from "react-router-dom";
 
 function App() {
     const user = useAuthUser();
+    const tenantsState = useTenantsState();
+    const { requestAuthorize } = useAuthActions();
     const history = useHistory();
     // const { user, isAuthenticated } = useAuth(); // Use this to avoid redirect to /account/login
 
@@ -17,6 +19,12 @@ function App() {
             </div>
             <div>
                 <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+            </div>
+            <div>
+                <button onClick={() => alert(JSON.stringify(tenantsState.tenants))}>Show my tenants</button>
+            </div>
+            <div>
+                <button onClick={() => requestAuthorize()}>Refresh my authentication</button>
             </div>
             <div>
                 <button onClick={() => history.push('/account/logout')}>Click me to logout</button>
